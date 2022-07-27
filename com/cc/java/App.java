0f965cdfa4dcf734d35cada9c6cd651268950dd5
Robert <robelix@gmx.de>
2022-07-27 10:51:39 +0200

    Flyable / IFeathers

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,14 +3,15 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+    
+        Bird bird = new Bird();
+        output(bird.fly());
+        output(bird.hasFeathers());
     }
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
     }
-
-
 
 }
 
